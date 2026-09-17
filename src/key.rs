fb2a2d500a3403b22dc9f30e89ccd4367c735d51
FlorianRoudaut <florian.roudaut@ubisoft.com>
2026-09-17 09:32:30 +0200

    Change key id to uuid + removed some unused directives

--- src/key.rs
+++ src/key.rs
@@ -1,8 +1,9 @@
 use serde::{Deserialize, Serialize};
+use uuid::Uuid;
 
 #[derive(Serialize, Deserialize)]
 pub struct Key {
-    pub id: String,
+    pub id: Uuid,
     pub version: u64,
     pub unique_name: String,
 }
--- src/lib.rs
+++ src/lib.rs
@@ -7,15 +7,10 @@ mod stock;
 mod exchange;
 mod position;
 
-#[allow(unused_imports)]
 pub use security::Security;
-#[allow(unused_imports)]
 pub use key::Key;
-#[allow(unused_imports)]
 pub use persisted::Persisted;
 pub use cash::{Cash, CASH};
-#[allow(unused_imports)]
 pub use stock::{Stock, STOCK};
-#[allow(unused_imports)]
 pub use exchange::{Exchange, EXCHANGE};
 pub use position::Position;
